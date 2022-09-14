f3a555c16424e2b1e6490592bb9587af8532e8ee
aht33 <alexhtang03@gmail.com>
2022-09-13 21:49:10 -0500

    first commit

---
+++ app/src/main/java/com/example/flas/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.flas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardQuestion.setOnClickListener {
+                flashcardQuestion.visibility = View.INVISIBLE
+                flashcardAnswer.visibility = View.VISIBLE
+            }
+        flashcardAnswer.setOnClickListener {
+            flashcardAnswer.visibility = View.INVISIBLE
+            flashcardQuestion.visibility = View.VISIBLE
+        }
+        }
+}
