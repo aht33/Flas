14b9be23e40a1bed3dee36a045409a0fa03f4b88
aht33 <alexhtang03@gmail.com>
2022-10-03 21:57:35 -0500

    Lab 2

---
+++ app/src/main/java/com/example/flas/AddCardActivity.kt
@@ -0,0 +1,65 @@
+package com.example.flas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class AddCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+
+        val cancelButton = findViewById<ImageView>(R.id.cancel_button)
+        cancelButton.setOnClickListener {
+            this.finish()
+        }
+
+        val questionText = findViewById<EditText>(R.id.questionField)
+        val answerText1 = findViewById<EditText>(R.id.answerField)
+        val answerText2 = findViewById<EditText>(R.id.answerField2)
+        val answerText3 = findViewById<EditText>(R.id.answerField3)
+
+        val saveButton = findViewById<ImageView>(R.id.saveButton)
+        saveButton.setOnClickListener {
+            val questionString = questionText.text.toString()
+            val answerString1 = answerText1.text.toString()
+            val answerString2 = answerText2.text.toString()
+            val answerString3 = answerText3.text.toString()
+            val data = Intent()
+            data.putExtra("QUESTION", questionString)
+            data.putExtra("ANSWER1", answerString1)
+            data.putExtra("ANSWER2", answerString2)
+            data.putExtra("ANSWER3", answerString3)
+
+            if(questionString == "" || answerString1 == "" || answerString2 == "" || answerString3 == "") {
+               val toast = Toast.makeText(applicationContext, "Fill in all of the blanks!", Toast.LENGTH_SHORT)
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+                toast.show()
+            } else {
+                setResult(RESULT_OK, data)
+                finish()
+            }
+
+        }
+
+        val questionToEdit = intent.getStringExtra("EQUESTION")
+        val answerToEdit1 = intent.getStringExtra("EANSWER1")
+        val answerToEdit2 = intent.getStringExtra("EANSWER2")
+        val answerToEdit3 = intent.getStringExtra("EANSWER3")
+        val questionField1 = findViewById<EditText>(R.id.questionField)
+        val answerField1 = findViewById<EditText>(R.id.answerField)
+        val answerField2 = findViewById<EditText>(R.id.answerField2)
+        val answerField3 = findViewById<EditText>(R.id.answerField3)
+        questionField1.setText(questionToEdit)
+        answerField1.setText(answerToEdit1)
+        answerField2.setText(answerToEdit2)
+        answerField3.setText(answerToEdit3)
+
+
+    }
+}
--- app/src/main/java/com/example/flas/MainActivity.kt
+++ app/src/main/java/com/example/flas/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.flas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
@@ -55,5 +59,52 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+
+            val data: Intent? = result.data
+
+            if (data != null) {
+                val questionString = data.getStringExtra("QUESTION")
+                val answerString1 = data.getStringExtra("ANSWER1")
+                val answerString2 = data.getStringExtra("ANSWER2")
+                val answerString3 = data.getStringExtra("ANSWER3")
+
+                flashcardQuestion.text = questionString
+                answerChoice1.text = answerString1
+                answerChoice2.text = answerString2
+                answerChoice3.text = answerString3
+
+
+                Log.i("Alex:Main Activity", "question: $questionString")
+                Log.i("Alex:Main Activity", "question $answerString1")
+
+                Snackbar.make(findViewById(R.id.flashcard_question), "Card successfully created",
+                    Snackbar.LENGTH_SHORT).show()
+            } else {
+                Log.i("Alex:Main Activity", "Returned null data from AddCardActivity")
+            }
+        }
+
+        val addFlashcardButton = findViewById<ImageView>(R.id.add_question_button)
+        addFlashcardButton.setOnClickListener {
+            val add = Intent(this, AddCardActivity::class.java)
+            resultLauncher.launch(add)
+        }
+
+        val editFlashcardButton = findViewById<ImageView>(R.id.edit_button)
+        editFlashcardButton.setOnClickListener {
+            val editQuestionString1 = flashcardQuestion.text.toString()
+            val answerString1 = answerChoice1.text.toString()
+            val answerString2 = answerChoice2.text.toString()
+            val answerString3 = answerChoice3.text.toString()
+            Intent(this, AddCardActivity::class.java).also {
+                it.putExtra("EQUESTION", editQuestionString1)
+                it.putExtra("EANSWER1", answerString1)
+                it.putExtra("EANSWER2", answerString2)
+                it.putExtra("EANSWER3", answerString3)
+                startActivity(it)
+            }
+        }
         }
 }
